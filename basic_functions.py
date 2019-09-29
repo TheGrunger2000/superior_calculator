3eaf67f2e59437998a23512c4a7e4a2f9f05abdb
Bogdan <grungerdiary@gmail.com>
2019-09-29 21:34:16 +0300

    29.09.2019: Functions (pass)

---
+++ basic_functions.py
@@ -0,0 +1,28 @@
+# Superior Calculator v 0.1.0
+# Created by TheGrunger2000
+# E-mail: grungerdiary@gmail.com
+from decimal import Decimal
+
+
+def summate():  # 2 + 3 = 2 + 1 + 1 + 1 = 5
+    pass
+
+
+def multiplicate():  # 2 * 3 = 2 + 2 + 2 = 6
+    pass
+
+
+def exponentiate():  # 2 ** 3 =  2 * 2 * 2 = 8
+    pass
+
+
+def tetrate():  # 2 ^^ 3 = 2 ** 2 ** 2 = 16
+    pass
+
+
+def pentate():  # 2 ^^^ 3 = 2 ^^ 2 ^^ 2 = 65 536
+    pass
+
+
+def hyperoperate():
+    pass
